cfaa80716e5ea6e15e7681e7bf52593f9f79dd41
Justine Evans <justine.evans@umontana.ed>
2018-05-24 16:22:25 -0600

    Add hw alert file.

--- js/hw-alert.js
+++ js/hw-alert.js
@@ -1,1 +1,1 @@
-alert("This is a homework example. \nMake sure to view the source code to see how it functions!");
+alert("This is a homework example. \nMake sure to view the source code to see how it was made, and access any associated files (like stylesheets and images).");
