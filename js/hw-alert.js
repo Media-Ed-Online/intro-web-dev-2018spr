a6ec899a83c4c82dc4c21ea4302449098c456651
Justine Evans <justine.evans@umontana.ed>
2018-05-24 17:09:25 -0600

    Updated hw-alert.js file.

--- js/hw-alert.js
+++ js/hw-alert.js
@@ -1,1 +1,1 @@
-alert("This is a homework example.\n\nMake sure to select “view source code” to see how it was made!\n\nYou can also access any associated files (like stylesheets) through this source.");
+alert("THIS IS A HOMEWORK EXAMPLE.\n\nMake sure to right-click and select “view source code” to see how it was made!\n\nYou can also access any associated files (like stylesheets) through this source.");
