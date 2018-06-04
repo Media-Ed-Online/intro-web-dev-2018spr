6b8a874af66bd8ba4a1a6a863f560542b086ce48
Justine Evans <justine.evans@umontana.ed>
2018-06-04 14:42:27 -0600

    Updated hw alert.

--- js/hw-alert.js
+++ js/hw-alert.js
@@ -1,1 +1,1 @@
-alert("THIS IS A HOMEWORK EXAMPLE.\n\nMake sure to right-click and select “view source code” to see how it was made!\n\nYou can also access any associated files (like stylesheets) through this source.");
+alert("THIS IS A HOMEWORK EXAMPLE.\n\nPlease note that your site may not use all of these elements.\n\nMake sure to right-click and select “view source code” to see how it was made!\n\nYou can also access any associated files (like stylesheets) through this source.");
